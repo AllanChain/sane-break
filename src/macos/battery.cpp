2a5956bcdb31dcc93c75d08119b79d04bb164aa4
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2024-09-19 20:38:12 +0800

    add battery monitoring on macOS

--- src/macos/battery.cpp
+++ src/macos/battery.cpp
@@ -7,6 +7,35 @@
 #include <qglobal.h>
 
 #ifdef Q_OS_MACOS
+#include <IOKit/ps/IOPSKeys.h>
+#include <IOKit/ps/IOPowerSources.h>
 
-bool DarwinBatteryStatus::systemOnBattery() { return false; }
+bool DarwinBatteryStatus::systemOnBattery() {
+  // Get the power source information
+  CFTypeRef powerSourceInfo = IOPSCopyPowerSourcesInfo();
+  CFArrayRef powerSourcesList = IOPSCopyPowerSourcesList(powerSourceInfo);
+
+  if (powerSourcesList) {
+    for (CFIndex i = 0; i < CFArrayGetCount(powerSourcesList); i++) {
+      CFTypeRef powerSource = CFArrayGetValueAtIndex(powerSourcesList, i);
+      CFDictionaryRef powerSourceDescription =
+          IOPSGetPowerSourceDescription(powerSourceInfo, powerSource);
+
+      if (powerSourceDescription) {
+        CFStringRef powerSourceState = (CFStringRef)CFDictionaryGetValue(
+            powerSourceDescription, CFSTR(kIOPSPowerSourceStateKey));
+
+        if (CFEqual(powerSourceState, CFSTR(kIOPSACPowerValue))) {
+          return false;
+        } else if (CFEqual(powerSourceState, CFSTR(kIOPSBatteryPowerValue))) {
+          return true;
+        }
+      }
+    }
+    CFRelease(powerSourcesList);
+  }
+  CFRelease(powerSourceInfo);
+
+  return false;
+}
 #endif
