265a0f3c9a98ea224c9bf876482e2ddebc1a6b7a
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2025-01-26 10:17:33 +0800

    fix welcome UI padding

--- src/welcome.cpp
+++ src/welcome.cpp
@@ -15,7 +15,6 @@
 #include <QVBoxLayout>
 #include <QWidget>
 #include <Qt>
-#include <utility>
 
 #ifdef Q_OS_LINUX
 #include "linux/system-check.h"
@@ -25,6 +24,7 @@ WelcomeWindow::WelcomeWindow(QWidget *parent) : QDialog(parent) {
   setWindowIcon(QIcon(":/images/icon_tray-color.png"));
   setWindowTitle("Welcome to Sane Break");
   setFixedWidth(400);
+  setContentsMargins(10, 20, 10, 10);
 
   QVBoxLayout *layout = new QVBoxLayout();
   setLayout(layout);
@@ -43,14 +43,14 @@ WelcomeWindow::WelcomeWindow(QWidget *parent) : QDialog(parent) {
   welcome->setText(
       "<h3 align=center>Welcome to Sane Break!</h3>"
       "<p>Sane Break is a cross-platform break reminder designed to help "
-      "you take meaningful breaks without disrupting your workflow."
+      "you take meaningful breaks without disrupting your workflow. "
       "Sane Break will stay in the system tray and remind you to take "
       "breaks at regular intervals. More details are available at <a "
       "href=\"https://github.com/AllanChain/sane-break/\">GitHub</a>.</p>");
   welcome->setOpenExternalLinks(true);
   welcome->setWordWrap(true);
+  welcome->setAlignment(Qt::AlignTop | Qt::AlignJustify);
   layout->addWidget(welcome);
-  layout->setAlignment(welcome, Qt::AlignTop | Qt::AlignJustify);
 
   bool hasWarningOrError = false;
 #ifdef Q_OS_LINUX
