16f6ac5f3a36d8c6c8930733c0a92f0315f6fb25
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2025-04-04 19:53:32 +0800

    add basic tests

---
+++ test/test-app.cpp
@@ -0,0 +1,151 @@
+// Sane Break is a gentle break reminder that helps you avoid mindlessly skipping breaks
+// Copyright (C) 2024-2025 Sane Break developers
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <gmock/gmock.h>
+#include <qtestcase.h>
+
+#include <QObject>
+#include <QSettings>
+#include <QTemporaryFile>
+#include <QTest>
+#include <QTimer>
+
+#include "lib/app-core.h"
+#include "lib/idle-time.h"
+#include "lib/preferences.h"
+
+using testing::Mock;
+using testing::NiceMock;
+
+class DummyIdleTime : public SystemIdleTime {
+ public:
+  using SystemIdleTime::SystemIdleTime;
+  void startWatching() {};
+  void stopWatching() {};
+  void setWatchAccuracy(int accuracy) {};
+  void setMinIdleTime(int idleTime) {};
+};
+
+class DummyApp : public AbstractApp {
+ public:
+  using AbstractApp::AbstractApp;
+  MOCK_METHOD(void, openBreakWindow, (bool), (override));
+  MOCK_METHOD(void, closeBreakWindow, (), (override));
+  MOCK_METHOD(void, updateTray, (), (override));
+  MOCK_METHOD(void, mayLockScreen, (), (override));
+  void advance(int secs) {
+    for (int i = 0; i < secs; i++) tick();
+  }
+};
+
+class TestApp : public QObject {
+  Q_OBJECT
+ private:
+  AppDependencies makeDeps() {
+    QTemporaryFile tempFile;
+    return {.countDownTimer = new ITimer(),
+            .oneshotIdleTimer = new DummyIdleTime(),
+            .screenLockTimer = new ITimer(),
+            .preferences = new SanePreferences(
+                new QSettings(tempFile.fileName(), QSettings::IniFormat))};
+  };
+
+ private slots:
+  void init() { QTest::failOnWarning(); }
+  void appInitialState() {
+    AppDependencies deps = makeDeps();
+    NiceMock<DummyApp> app(deps);
+    QCOMPARE(app.pauseReasons.toInt(), 0);
+    QCOMPARE(app.secondsToNextBreak, deps.preferences->smallEvery->get());
+    QCOMPARE(app.smallBreaksBeforeBig(), deps.preferences->bigAfter->get() - 1);
+  }
+  void tick() {
+    AppDependencies deps = makeDeps();
+    NiceMock<DummyApp> app(deps);
+    app.start();
+
+    QVERIFY(deps.countDownTimer->isActive());
+
+    EXPECT_CALL(app, updateTray()).Times(1);
+    emit deps.countDownTimer->timeout();
+    // Time has ellapsed
+    QCOMPARE(app.secondsToNextBreak, deps.preferences->smallEvery->get() - 1);
+    // But not paused time
+    QCOMPARE(app.secondsPaused, 0);
+    QVERIFY(Mock::VerifyAndClearExpectations(&app));
+  }
+  void showFirstBreak() {
+    AppDependencies deps = makeDeps();
+    NiceMock<DummyApp> app(deps);
+    app.start();
+
+    // Break window will show
+    EXPECT_CALL(app, openBreakWindow(false)).Times(1);
+    app.advance(app.secondsToNextBreak);
+    QCOMPARE(app.secondsToNextBreak, 0);
+    // Countdown is stopped
+    QVERIFY(!deps.countDownTimer->isActive());
+    QVERIFY(Mock::VerifyAndClearExpectations(&app));
+
+    // Simulate break window close
+    app.onBreakEnd();
+
+    // Countdown is running again
+    QVERIFY(deps.countDownTimer->isActive());
+    // The remaining time is correct
+    QCOMPARE(app.secondsToNextBreak, deps.preferences->smallEvery->get());
+  }
+  void showBigBreak() {
+    AppDependencies deps = makeDeps();
+    NiceMock<DummyApp> app(deps);
+    app.start();
+
+    int numberOfBreaks = deps.preferences->bigAfter->get();
+    EXPECT_CALL(app, openBreakWindow(false)).Times(numberOfBreaks - 1);
+    EXPECT_CALL(app, openBreakWindow(true)).Times(1);
+    for (int j = 0; j < numberOfBreaks; j++) {
+      app.advance(app.secondsToNextBreak);
+      app.onBreakEnd();
+    }
+    QVERIFY(Mock::VerifyAndClearExpectations(&app));
+  }
+  void idleAfterBreak() {
+    AppDependencies deps = makeDeps();
+    NiceMock<DummyApp> app(deps);
+    app.start();
+    app.advance(app.secondsToNextBreak);
+
+    deps.oneshotIdleTimer->isIdle = true;
+    emit deps.oneshotIdleTimer->idleStart();
+    app.onBreakEnd();
+
+    // Countdown is paused if user is idle after break
+    QVERIFY(!deps.countDownTimer->isActive());
+
+    deps.oneshotIdleTimer->isIdle = false;
+    emit deps.oneshotIdleTimer->idleEnd();
+
+    // Countdown resumed
+    QVERIFY(deps.countDownTimer->isActive());
+  }
+  void notIdleAfterBreak() {
+    AppDependencies deps = makeDeps();
+    NiceMock<DummyApp> app(deps);
+    app.start();
+    app.advance(app.secondsToNextBreak);
+
+    deps.oneshotIdleTimer->isIdle = true;
+    emit deps.oneshotIdleTimer->idleStart();
+    deps.oneshotIdleTimer->isIdle = false;
+    emit deps.oneshotIdleTimer->idleEnd();
+
+    app.onBreakEnd();
+
+    // Countdown resumed
+    QVERIFY(deps.countDownTimer->isActive());
+  }
+};
+
+QTEST_APPLESS_MAIN(TestApp)
+#include "test-app.moc"
