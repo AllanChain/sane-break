4d0c5b4fb89114b485a9803cb021c0c870414ccf
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2025-04-10 15:32:14 +0800

    merge tests into same file

--- test/test-app.cpp
+++ test/test-app.cpp
@@ -179,6 +179,7 @@ class TestApp : public QObject {
     int secondsToNextBreak = app.trayData.secondsToNextBreak;
     app.postpone(100);
     QCOMPARE(app.trayData.secondsToNextBreak, secondsToNextBreak + 100);
+    QCOMPARE(app.trayData.smallBreaksBeforeBigBreak, 0);
   }
   void pauseBreakOnIdle() {
     auto [deps, windowControl] = DummyApp::makeDeps();
@@ -253,6 +254,58 @@ class TestApp : public QObject {
     QCOMPARE(app.trayData.secondsToNextBreak, smallEvery - 1);
     QCOMPARE(app.trayData.smallBreaksBeforeBigBreak,
              deps.preferences->bigAfter->get() - 1);
+  }
+  void pauseWhileBreak() {
+    auto [deps, windowControl] = DummyApp::makeDeps();
+    deps.preferences->pauseOnBattery->set(true);
+    NiceMock<DummyApp> app(deps);
+    app.start();
+
+    EXPECT_CALL(*windowControl, createWindows(SaneBreak::BreakType::Small)).Times(1);
+    app.breakNow();
+    QVERIFY(Mock::VerifyAndClearExpectations(&app));
+
+    EXPECT_CALL(*windowControl, deleteWindows()).Times(1);
+    emit deps.systemMonitor->batteryPowered();
+    QVERIFY(Mock::VerifyAndClearExpectations(&app));
+  }
+  void idleWhileBreak() {
+    auto [deps, windowControl] = DummyApp::makeDeps();
+    NiceMock<DummyApp> app(deps);
+    app.start();
+
+    EXPECT_CALL(*windowControl, createWindows(SaneBreak::BreakType::Small)).Times(1);
+    app.breakNow();
+    EXPECT_CALL(*windowControl, deleteWindows()).Times(0);
+    emit deps.systemMonitor->idleStarted();
+
+    QVERIFY(Mock::VerifyAndClearExpectations(&app));
+  }
+  void moreThanOnePauseReasons() {
+    auto [deps, windowControl] = DummyApp::makeDeps();
+    deps.preferences->pauseOnBattery->set(true);
+    NiceMock<DummyApp> app(deps);
+    app.start();
+
+    emit deps.systemMonitor->batteryPowered();
+    emit deps.systemMonitor->idleStarted();
+
+    // Countdown stopped
+    int secondsToNextBreak = app.trayData.secondsToNextBreak;
+    app.advance(1);
+    QCOMPARE(app.trayData.secondsToNextBreak, secondsToNextBreak);
+    QVERIFY(app.trayData.pauseReasons.testFlags(SaneBreak::PauseReason::Idle |
+                                                SaneBreak::PauseReason::OnBattery));
+
+    emit deps.systemMonitor->idleEnded();
+    app.advance(1);
+    QCOMPARE(app.trayData.secondsToNextBreak, secondsToNextBreak);
+    QVERIFY(app.trayData.pauseReasons.testFlag(SaneBreak::PauseReason::OnBattery));
+
+    emit deps.systemMonitor->adaptorPowered();
+    app.advance(1);
+    QCOMPARE(app.trayData.secondsToNextBreak, secondsToNextBreak - 1);
+    QVERIFY(!app.trayData.pauseReasons);
   }
 };
 
