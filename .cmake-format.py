6f2c39419bae4476b4cbaeebc9a6d8a6a01486fe
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2025-05-03 23:04:39 +0800

    add license headers to script files

--- .cmake-format.py
+++ .cmake-format.py
@@ -1,3 +1,7 @@
+# Sane Break is a gentle break reminder that helps you avoid mindlessly skipping breaks
+# Copyright (C) 2024-2025 Sane Break developers
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 with section("format"):  # noqa
     line_width = 88
     tab_size = 2
