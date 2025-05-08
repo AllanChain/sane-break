712cde0241c33f597c5776fe6b3eaa39c615d967
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2025-05-08 09:42:45 +0800

    split screen-lock into different files
    And use dynamic library call for macOS lock screen

--- src/lib/linux/screen-lock.cpp
+++ src/lib/linux/screen-lock.cpp
@@ -2,31 +2,11 @@
 // Copyright (C) 2024-2025 Sane Break developers
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include <qglobal.h>
-
 #include <QProcess>
 
-#ifdef Q_OS_WIN
-#include <windows.h>
-#endif
-// https://stackoverflow.com/a/26492632/8810271
-#ifdef Q_OS_MAC
-extern "C" int SACLockScreenImmediate(void);
-#endif
-
 bool lockScreen() {
-#ifdef Q_OS_WIN
-  return LockWorkStation();
-#else
   QProcess process;
-
-#ifdef Q_OS_MAC
-  SACLockScreenImmediate();
-#elif defined Q_OS_LINUX
   process.start("xdg-screensaver", {"lock"});
-#endif
-
   process.waitForFinished();
   return process.exitCode() == 0;
-#endif
 }
---
+++ src/lib/macos/screen-lock.cpp
@@ -0,0 +1,41 @@
+// Sane Break is a gentle break reminder that helps you avoid mindlessly skipping breaks
+// Copyright (C) 2024-2025 Sane Break developers
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <dlfcn.h>
+
+#include <QDebug>
+#include <QProcess>
+
+bool lockScreenWithFramework() {
+  void* handle =
+      dlopen("/System/Library/PrivateFrameworks/login.framework/login", RTLD_LAZY);
+  if (!handle) {
+    qWarning() << "Error opening library: " << dlerror();
+    return false;
+  }
+  typedef void (*SACLockScreenImmediateFunc)();
+  SACLockScreenImmediateFunc func =
+      (SACLockScreenImmediateFunc)dlsym(handle, "SACLockScreenImmediate");
+  const char* dlsym_error = dlerror();
+  if (dlsym_error) {
+    qWarning() << "Error getting symbol: " << dlsym_error;
+    dlclose(handle);
+    return false;
+  }
+  func();
+  dlclose(handle);
+  return true;
+}
+
+bool lockScreenWithSleep() {
+  QProcess process;
+  process.start("pmset", {"displaysleepnow"});
+  process.waitForFinished();
+  return process.exitCode() == 0;
+}
+
+bool lockScreen() {
+  if (!lockScreenWithFramework()) return lockScreenWithSleep();
+  return true;
+}
