e7dd82ce7fe627c4ea39df2bd5fec2f2cbf0d968
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2025-05-19 12:11:57 +0800

    update clang-format hook

--- src/lib/linux/wayland/idle.h
+++ src/lib/linux/wayland/idle.h
@@ -32,14 +32,10 @@ class IdleTimeWayland : public SystemIdleTime {
   static void globalAdded(void *data, wl_registry *registry, uint32_t name,
                           const char *interface, uint32_t version);
   static void globalRemoved(void *data, wl_registry *registry, uint32_t name);
-  const struct wl_registry_listener globalListener {
-    globalAdded, globalRemoved
-  };
+  const struct wl_registry_listener globalListener{globalAdded, globalRemoved};
   static void idled(void *data, ext_idle_notification_v1 *object);
   static void resumed(void *data, ext_idle_notification_v1 *object);
-  const struct ext_idle_notification_v1_listener idleListener {
-    idled, resumed
-  };
+  const struct ext_idle_notification_v1_listener idleListener{idled, resumed};
   wl_seat *seat;
   ext_idle_notifier_v1 *idleNotifier = nullptr;
   ext_idle_notification_v1 *idleNotification = nullptr;
