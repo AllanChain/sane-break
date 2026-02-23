2246c4e1c41c3e091a0a8064a43038dadcd48536
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2026-02-23 14:28:47 +0800

    support color selection dialog

--- src/app/widgets/color-edit.h
+++ src/app/widgets/color-edit.h
@@ -1,11 +1,16 @@
 // Sane Break is a gentle break reminder that helps you avoid mindlessly skipping breaks
-// Copyright (C) 2024-2025 Sane Break developers
+// Copyright (C) 2024-2026 Sane Break developers
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #pragma once
 
+#include <QColorDialog>
+#include <QEvent>
 #include <QLineEdit>
+#include <QPainter>
+#include <QPalette>
 #include <QPixmap>
+#include <QString>
 #include <QWidget>
 
 class ColorEdit : public QLineEdit {
@@ -13,13 +18,32 @@ class ColorEdit : public QLineEdit {
  public:
   ColorEdit(QWidget* parent = nullptr) : QLineEdit(parent) {
     setInputMask("\\#HHHHHHHH");
-    connect(this, &QLineEdit::textChanged, this, [this]() {
-      QPixmap icon(10, 10);
-      icon.fill(this->text());
-      if (actions().empty())
-        addAction(icon, QLineEdit::TrailingPosition);
-      else
-        actions()[0]->setIcon(icon);
-    });
+    m_colorAction = addAction(QIcon(), QLineEdit::TrailingPosition);
+    connect(this, &QLineEdit::textChanged, this, &ColorEdit::updateIcon);
+    connect(m_colorAction, &QAction::triggered, this, &ColorEdit::pickColor);
   };
+
+ protected:
+  void changeEvent(QEvent* event) override {
+    QLineEdit::changeEvent(event);
+    if (event->type() == QEvent::PaletteChange) updateIcon();
+  }
+
+ private:
+  QAction* m_colorAction;
+
+  void updateIcon() {
+    QPixmap icon(10, 10);
+    QPainter painter(&icon);
+    painter.setPen(palette().color(QPalette::Text));
+    painter.setBrush(QColor(text()));
+    painter.drawRect(icon.rect().adjusted(0, 0, -1, -1));
+    m_colorAction->setIcon(icon);
+  }
+
+  void pickColor() {
+    QColor color = QColorDialog::getColor(QColor(text()), this, QString(),
+                                          QColorDialog::ShowAlphaChannel);
+    if (color.isValid()) setText(color.name(QColor::HexArgb));
+  }
 };
